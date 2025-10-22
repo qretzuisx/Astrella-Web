98ca1fe0cda59eaf9cfd12819d1a2ecd4769c451
Eureka <rhizalynespiritu@gmail.com>
2025-10-22 23:40:11 +0800

    Initial Commit

--- astrella-web/front-end/src/components/Navbar.jsx
+++ astrella-web/front-end/src/components/Navbar.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {navbarStyles as styles} from '../rentalstyles'
 
 const Navbar = () => {
+
+    const [scrolled, setScrolled] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
+
+      const navLinks = [
+    { to: '/', label: 'Home'},
+    { to: '/', label: 'Gowns'},
+    {to: '/', label: 'Contacts'},
+    ];
+
   return (
-    <div>Navbar</div>
+    <nav className={'$'}></nav>
   )
 }
 
