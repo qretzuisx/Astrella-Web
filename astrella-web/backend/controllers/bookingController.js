9583e8a219d43fd9796c381aa9f01f2972b88d88
Eureka <rhizalynespiritu@gmail.com>
2025-10-26 19:05:31 +0800

    Commits on modified Booking APIs

--- astrella-web/backend/controllers/bookingController.js
+++ astrella-web/backend/controllers/bookingController.js
@@ -1,14 +1,57 @@
-import Booking from "../models/booking.js"
+import Booking from "../models/booking.js";
+import Gown from "../models/Gown.js";
 
+// API reserving a gown
+export const createBooking = async (req, res)=> {
+    try {
+        const {_id} = req.user;
+        const {gown} = req.body;
+     
+        const gownData = await Gown.findById(gown);
+        if(!gownData) {
+            return res.json({ success: false, message: "Gown not Found"});
+        }
 
-// to check availability of gown for given date
-const checkAvailability = async (gown, startDate, endDate)=>{
-    const bookings = await Booking.find({
-        gown,
-        startDate: {$lte: endDate},
-        endDate: {$lte: startDate},
-    })
-    return bookings.length === 0;
+        const isAlreadyReserved = await Booking.findOne({gown, status: {$in: ["reserved", "confirmed"]}});
+        if(isAlreadyReserved) {
+            return res.json({success: false, message: "Gown is already reserved"});
+        }
+
+        const newBooking = await Booking.create({user: _id, gown, owner: gownData.owner, status:"reserved"});
+        res.json({ success: true, message: "Gown reserved successfully. You will be notified by owner for pickup", data: newBooking});
+    } catch (error) {
+        console.log(error.message);
+        res.json({success: false, message: error.message});
+    }
+};
+
+// API to list user bookings
+export const getUserBookings = async (req, res)=> {
+    try {
+        const {_id} = req.user;
+        const bookings = await Booking.find({ user: _id}).populate("gown", "name image") .populate("owner", "name email");
+        res.json({ success: true, bookings });
+
+    } catch (error) {
+        console.log(error.message);
+        res.json({ success: false, message: error.message});
+    }
 }
 
-// API to recommend best styles 
+// API to get owner bookings
+export const getOwnerBookings = async (req, res)=> {
+    try {
+        if(req.user.role !== 'owner'){
+            return res.json({ success: false, message: "Unauthorized"});
+        }
+
+
+        const {_id} = req.user;
+        const booking = await Booking.find({ owner: _id}).populate("user", "name email").populate("gown", "name image");
+        res.json({success: true, bookings });
+       
+
+    } catch (error) {
+         res.json({ succes: false, message: error.message});
+    }
+}
--- astrella-web/backend/models/booking.js
+++ astrella-web/backend/models/booking.js
@@ -5,12 +5,11 @@ const bookingSchema = new mongoose.Schema({
     gown: {type: ObjectId, ref: "Gown", required: true},
     user: {type: ObjectId, ref: "User", required: true},
     owner: {type: ObjectId, ref: "User", required: true},
-    startDate: {type: Date, required: true},
-    endDate: {type: Date, required: true},
-    totalPrice: {type: Number, required: true},
-    status: {type: String, enum: ["pending", "completed", "confirmed"], default: "pending"}
+    status: {type: String, enum: ["pending", "picked_up", "confirmed", "rejected"], default: "pending"},
+    pickupDate: {type: Date},
+    createdAt: {type: Date, default: Date.now},
+    updatedAt: {type: Date,}
 }, {timestamps: true})
 
 const Booking = mongoose.model('Booking', bookingSchema)
-
-export default Booking
+export default Booking;
