9a1b794cc76479aec4f17f25173ac1124b670918
Eureka <rhizalynespiritu@gmail.com>
2025-10-26 08:39:45 +0800

    Initial commits on the User Management

---
+++ astrella-web/backend/controllers/userController.js
@@ -0,0 +1,68 @@
+import User from "../models/User.js"
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+
+
+// jwt token
+const generateToken = (userId)=>{
+    const payload = userId;
+    return jwt.sign(payload, process.env.JWT_SECRET)
+}
+
+// register user
+export const registerUser = async (req, res)=>{
+    try {
+        const {name, email, password} = req.body
+
+        if(!name || !email || !password || password.length < 8){
+            return res.json({sucess: false, message: 'Fill all the Fields !'})
+        }
+
+        const userExists = await User.findOne({email})
+        if(userExists){
+            return res.json({sucess: false, message: 'User already exists'})
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10)
+        const user = await User.create({name, email, password: hashedPassword})
+        const token = generateToken(user._id.toString())
+        res.json({sucess: true, token})
+
+
+    } catch (error) {
+        console.log(error.message);
+        res.json({sucess: false, message: error.message})
+    }
+}
+
+// login user
+export const loginUser = async (req, res)=>{
+    try {
+        const {email, password} = req.body
+        const user = await User.findOne({email})
+        if(!user){
+            return res.json({sucess: false, message: "User not found"})
+        }
+        const isMatch = await bcrypt.compare(password, user.password)
+        if(!isMatch){
+            return res.json({sucess: false, message: "Invalid Credentials"})
+        }
+        const token = generateToken(user._id.toString())
+        res.json({sucess: true, token})
+    
+    } catch (error) {
+        console.log(error.message);
+        res.json({sucess: false, message: error.message})
+    }
+}
+
+// user data using jwt
+export const getUserData = async (req, res) =>{
+    try {
+        const {user} = req;
+        res.json({sucess: true, user})
+    } catch (error) {
+        console.log(error.message);
+        res.json({sucess:false, message: error.message})
+    }
+}
---
+++ astrella-web/backend/server.js
@@ -0,0 +1,22 @@
+import express, { response } from "express";
+import "dotenv/config";
+import cors from "cors";
+import connectDb from "./configs/db.js";
+import userRouter from "./routes/userRoutes.js";
+
+const app = express()
+
+
+// connect database
+await connectDb()
+
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => res.send("Server is running"))
+app.use('/api/user', userRouter)
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`));
