cd70244035e987f430f79f3dbf3c35cb59e47ee2
Rhizalyn <rhizalynespiritu@gmail.com>
2025-10-29 12:31:55 +0800

    Commits on debugging and implementing verification on the user if its owner

--- backend/routes/ownerRoutes.js
+++ backend/routes/ownerRoutes.js
@@ -1,18 +1,17 @@
 import express from "express";
 import { protect } from "../middleware/auth.js";
-import { addGown, changeRoleToOwner, DeleteGown, getDashboardData, getOwnersGowns, ToggleGownAvailability, updateUserImage } from "../controllers/ownerController.js";
+import { addGown, DeleteGown, getDashboardData, getOwnersGowns, ToggleGownAvailability, updateUserImage } from "../controllers/ownerController.js";
 import upload from "../middleware/multer.js";
+import { verifyOwner } from "../middleware/verify.js/index.js";
 
 const ownerRouter = express.Router();
 
 
-
-ownerRouter.post("/change-role", protect, changeRoleToOwner)
-ownerRouter.post("/add-gown", protect, upload.single("image"), addGown)
-ownerRouter.get("/gowns", protect, getOwnersGowns)
-ownerRouter.post("/toogle-gown", protect, ToggleGownAvailability)
-ownerRouter.post("/delete-gown", protect, DeleteGown)
-ownerRouter.get("/dashboard", protect, getDashboardData)
-ownerRouter.post("/update-image", protect, upload.single("image"), updateUserImage)
+ownerRouter.post("/add-gown", protect, verifyOwner, upload.single("image"), addGown)
+ownerRouter.get("/gowns", protect, verifyOwner, getOwnersGowns)
+ownerRouter.put("/toogle-gown", protect, verifyOwner, ToggleGownAvailability)
+ownerRouter.delete("/delete-gown", protect, verifyOwner, DeleteGown)
+ownerRouter.get("/dashboard", protect, verifyOwner, getDashboardData)
+ownerRouter.post("/update-image", protect, verifyOwner, upload.single("image"), updateUserImage)
 
 export default ownerRouter;
